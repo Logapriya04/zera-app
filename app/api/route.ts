0aedc46af7d7bf99773550151c6d110d1265e7bb
Logapriya04 <144525278+Logapriya04@users.noreply.github.com>
2025-11-26 23:48:18 +0530

    Refactor GET function to use NextResponse

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,12 @@
-export async function GET(params:JSON) {
-    return Response.json({message: 'Hello, from Backend!', data: params});
-    
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  // You can read query params if needed
+  const url = new URL(request.url);
+  const params = Object.fromEntries(url.searchParams.entries());
+
+  return NextResponse.json({
+    message: "Hello, from Backend!",
+    data: params,
+  });
 }
